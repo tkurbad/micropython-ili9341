1fdc758ee12cef9c6c469ab1de9b9e6f7ca4fdb2
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2020-10-06 18:06:49 +0200

    Archived

--- examples/01_basic/04a_fillMonocolor.py
+++
@@ -1,8 +0,0 @@
-# fillMonocolor allows to fill the screen with a color at RGB565 format.
-#
-from ili9341.lcd import *
-
-l = LCD(baud = 21000000) # step down the SPI bus speed to 21 MHz may be opportune when using 150+ mm wires
-
-# Completely fill the screen in green
-l.fillMonocolor(GREEN)
